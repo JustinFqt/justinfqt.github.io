fe267b803543ac213d4231bc79a16cc26c8216aa
JustinFqt <75577798+JustinFqt@users.noreply.github.com>
2025-03-22 14:16:44 +0100

    upgrade tailwindcss

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <a
-  class="inline-block mx-2 px-8 py-3 text-sm font-medium text-blue-900 transition border border-current rounded hover:scale-110 hover:rotate-2 active:text-blue-500 focus:outline-none focus:ring dark:text-white"
+  class="inline-block mx-2 px-8 py-3 text-sm font-medium text-blue-900 transition border border-current rounded-sm hover:scale-110 hover:rotate-2 active:text-blue-500 focus:outline-hidden focus:ring-3 dark:text-white"
   href={url}
 >
   {logo}
